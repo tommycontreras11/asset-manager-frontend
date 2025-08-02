2bd344a329dd0e15761f522cdb592d5a48c79d9f
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-08-02 17:28:26 -0400

    feat: :sparkles: update eslint configuration to disable specific TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    // Apply these overrides to all TypeScript and JavaScript files
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "unused-imports/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
